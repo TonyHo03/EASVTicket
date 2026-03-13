2979b4fd7c0924b12791e79154af7c8334b31ec0
Alv3s04 <lukas.alves.christensen@gmail.com>
2026-03-13 11:35:28 +0100

    Interfaces implemented in DAOs.

--- src/main/java/dk/easv/easvticket/DAL/DAO/EventDAO.java
+++ src/main/java/dk/easv/easvticket/DAL/DAO/EventDAO.java
@@ -1,4 +1,28 @@
 package dk.easv.easvticket.DAL.DAO;
 
-public class EventDAO {
+import dk.easv.easvticket.BE.Event;
+import dk.easv.easvticket.DAL.Interfaces.IEventDataAccess;
+
+import java.util.List;
+
+public class EventDAO implements IEventDataAccess {
+    @Override
+    public Event createEvent(Event newEvent) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Event> getEvents() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public void updateEvent(Event event) throws Exception {
+
+    }
+
+    @Override
+    public void deleteEvent(int eventID) throws Exception {
+
+    }
 }
